8ccc23fd17bf913fa4d7caa0b548b58a5bf60a01
pjaermann <p.jaermann@gmail.com>
2016-04-19 14:17:27 +0200

    create digits 0->9 mpin ap to print the desired digit on the 7segment

--- oneShiftRegTest.c
+++ oneShiftRegTest.c
@@ -1,6 +1,6 @@
 #include <avr/io.h>
 
-//#define F_CPU 8000000
+#define F_CPU 8000000
 #include <util/delay.h>
 
 #define DS_PORT		PORTC
@@ -16,22 +16,34 @@
 #define ST_CP_high()	ST_CP_PORT|=_BV(ST_CP_PIN)
 #define SH_CP_low()	SH_CP_PORT&=~_BV(SH_CP_PIN)
 #define SH_CP_high()	SH_CP_PORT|=_BV(SH_CP_PIN)
-
-void write_digit(unsigned char digit);
+//map of the digits 0->9 BASED on the connection between the shift register and the 7 segments
+uint8_t digits[10]={
+	0b01111110,
+	0b00010010,
+	0b10111100,
+	0b10110110,
+	0b11010010,
+	0b11100110,
+	0b11001110,
+	0b00110010,
+	0b11111110,
+	0b11110110
+};
+void write_digit(uint8_t digit);
 int main (void){
 	DDRC = 0b00000111;
 	PORTC= 0b00000000;
 	while(1){
 		
-		for(uint8_t i=0;i<=7;i++){
-			write_digit(_BV(i));
-			_delay_ms(500);
+		for(uint8_t i=0;i<9;i++){
+			write_digit(digits[i]);
+			_delay_ms(100);
 		}
 
 	}
 	return 1;
 }
-void write_digit(unsigned char digit){
+void write_digit(uint8_t digit){
 	SH_CP_low();
 	ST_CP_low();
 	for(uint8_t i=0;i<8;i++){
